b2cb449d82d35b4dbe61f64be411294acc7aa853
mc-jenia <azoz1998.a1@gmail.com>
2022-07-10 20:40:18 +0300

    setup files

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+import 'providers/great_places_provider.dart';
+import 'screens/places_list_screen.dart';
+
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return ChangeNotifierProvider.value(
+      value: GreatPlacesProvider(),
+      child: MaterialApp(
+        title: 'Great Places',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch(
+            primarySwatch: Colors.indigo,
+          ).copyWith(
+            secondary: Colors.amber,
+          ),
+        ),
+        home: const PlacesListScreen(),
       ),
-      home: null,
     );
   }
 }
